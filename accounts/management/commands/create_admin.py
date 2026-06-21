6dce2fc79321d0ef0fa5adfc4549f1b53c703c0e
Isyaku Murtala Abdulhamid <52487641+Muri68@users.noreply.github.com>
2026-06-21 23:11:13 +0100

    updated requirement.txt

--- accounts/management/commands/create_admin.py
+++ accounts/management/commands/create_admin.py
@@ -5,9 +5,9 @@ class Command(BaseCommand):
     help = 'Creates a super admin user'
 
     def handle(self, *args, **options):
-        if not CustomUser.objects.filter(email='admin@jobportal.com').exists():
+        if not CustomUser.objects.filter(email='admin@datacentrerecruit.com').exists():
             admin = CustomUser.objects.create_superuser(
-                email='admin@jobportal.com',
+                email='admin@datacentrerecruit.com',
                 password='Admin@123',
                 first_name='Admin',
                 last_name='User',
